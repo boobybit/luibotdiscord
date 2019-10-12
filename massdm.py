aaf69be17b55ca3fc839461e98befc4404b25520
boobybit <47494084+boobybit@users.noreply.github.com>
2019-10-12 19:28:52 +0700

    Add files via upload

---
+++ massdm.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+TOKEN = "user token"
+BOT_PREFIX = ("&")
+bot = commands.Bot(command_prefix=BOT_PREFIX, description='Hi')
+
+@bot.command(pass_context=True)
+async def userping(ctx, message):
+    for members in bot.get_all_members():
+        try:
+
+                embed = discord.Embed(title="Epic title", description=message, color=ctx.message.author.top_role.colour)
+                embed.set_footer(text="your daddy▪️ {}".format(ctx.message.created_at)[:-10], icon_url=(ctx.message.author.avatar_url))
+
+                y=0
+                x = y
+                y = x + 1
+                await members.send(embed=embed)
+                await ctx.send("Sended {} messages!".format(y))
+        except:
+            continue
+
+
+bot.run('bot token')
