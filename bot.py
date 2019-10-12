d4dc0fe357ea9b16790595bceafe9a0fe25fcaf3
boobybit <47494084+boobybit@users.noreply.github.com>
2019-10-12 19:41:08 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 
-TOKEN = "NjI5MTU1OTg3NDIwMzQ4NDE2.XaGwhQ.zicmivcccJwafNV7cB6p2TSCO8o"
+TOKEN = "NjI5MTU1OTg3NDIwMzQ4NDE2.XaHJcA.3wzzXFM3ZEwiTmo4Ln-hKC3WBug"
 BOT_PREFIX = ("&")
 bot = commands.Bot(command_prefix=BOT_PREFIX, description='Hi')
 
@@ -16,4 +16,4 @@ async def userping(ctx):
         	print("Error_00x58880: Close This! Bot spammed")
 
 
-bot.run('NjI5MTU1OTg3NDIwMzQ4NDE2.XaGwhQ.zicmivcccJwafNV7cB6p2TSCO8o')
+bot.run('NjI5MTU1OTg3NDIwMzQ4NDE2.XaHJcA.3wzzXFM3ZEwiTmo4Ln-hKC3WBug')
